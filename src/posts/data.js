c49cd0d9ca7b37598fc7ee6bf0e5ad495e7af45f
Phyllis Feng <phyllisfeng@phylliss-mbp.wifi.local.cmu.edu>
2023-09-02 16:21:13 -0400

    changed import statements and intFields part

--- src/posts/data.js
+++ src/posts/data.js
@@ -1,60 +1,119 @@
-'use strict';
-
-const db = require('../database');
-const plugins = require('../plugins');
-const utils = require('../utils');
-
+"use strict";
+// const db = require('../database');
+// const plugins = require('../plugins');
+// const utils = require('../utils');
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
+};
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const db = __importStar(require("../database"));
+const plugins = __importStar(require("../plugins"));
+const utils = __importStar(require("../utils"));
+// const intFields = [
+//     'uid', 'pid', 'tid', 'deleted', 'timestamp',
+//     'upvotes', 'downvotes', 'deleterUid', 'edited',
+//     'replies', 'bookmarks',
+// ];
 const intFields = [
     'uid', 'pid', 'tid', 'deleted', 'timestamp',
     'upvotes', 'downvotes', 'deleterUid', 'edited',
     'replies', 'bookmarks',
 ];
-
 module.exports = function (Posts) {
-    Posts.getPostsFields = async function (pids, fields) {
-        if (!Array.isArray(pids) || !pids.length) {
-            return [];
-        }
-        const keys = pids.map(pid => `post:${pid}`);
-        const postData = await db.getObjects(keys, fields);
-        const result = await plugins.hooks.fire('filter:post.getFields', {
-            pids: pids,
-            posts: postData,
-            fields: fields,
+    Posts.getPostsFields = function (pids, fields) {
+        return __awaiter(this, void 0, void 0, function* () {
+            if (!Array.isArray(pids) || !pids.length) {
+                return [];
+            }
+            const keys = pids.map(pid => `post:${pid}`);
+            const postData = yield db.getObjects(keys, fields);
+            const result = yield plugins.hooks.fire('filter:post.getFields', {
+                pids: pids,
+                posts: postData,
+                fields: fields,
+            });
+            result.posts.forEach(post => modifyPost(post, fields));
+            return result.posts;
         });
-        result.posts.forEach(post => modifyPost(post, fields));
-        return result.posts;
     };
-
-    Posts.getPostData = async function (pid) {
-        const posts = await Posts.getPostsFields([pid], []);
-        return posts && posts.length ? posts[0] : null;
+    Posts.getPostData = function (pid) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const posts = yield Posts.getPostsFields([pid], []);
+            return posts && posts.length ? posts[0] : null;
+        });
     };
-
-    Posts.getPostsData = async function (pids) {
-        return await Posts.getPostsFields(pids, []);
+    Posts.getPostsData = function (pids) {
+        return __awaiter(this, void 0, void 0, function* () {
+            return yield Posts.getPostsFields(pids, []);
+        });
     };
-
-    Posts.getPostField = async function (pid, field) {
-        const post = await Posts.getPostFields(pid, [field]);
-        return post ? post[field] : null;
+    Posts.getPostField = function (pid, field) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const post = yield Posts.getPostFields(pid, [field]);
+            return post ? post[field] : null;
+        });
     };
-
-    Posts.getPostFields = async function (pid, fields) {
-        const posts = await Posts.getPostsFields([pid], fields);
-        return posts ? posts[0] : null;
+    Posts.getPostFields = function (pid, fields) {
+        return __awaiter(this, void 0, void 0, function* () {
+            const posts = yield Posts.getPostsFields([pid], fields);
+            return posts ? posts[0] : null;
+        });
     };
-
-    Posts.setPostField = async function (pid, field, value) {
-        await Posts.setPostFields(pid, { [field]: value });
+    Posts.setPostField = function (pid, field, value) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield Posts.setPostFields(pid, { [field]: value });
+        });
     };
-
-    Posts.setPostFields = async function (pid, data) {
-        await db.setObject(`post:${pid}`, data);
-        plugins.hooks.fire('action:post.setFields', { data: { ...data, pid } });
+    Posts.setPostFields = function (pid, data) {
+        return __awaiter(this, void 0, void 0, function* () {
+            yield db.setObject(`post:${pid}`, data);
+            plugins.hooks.fire('action:post.setFields', { data: Object.assign(Object.assign({}, data), { pid }) });
+        });
     };
 };
-
+// function modifyPost(post, fields) {
+//     if (post) {
+//         db.parseIntFields(post, intFields, fields);
+//         if (post.hasOwnProperty('upvotes') && post.hasOwnProperty('downvotes')) {
+//             post.votes = post.upvotes - post.downvotes;
+//         }
+//         if (post.hasOwnProperty('timestamp')) {
+//             post.timestampISO = utils.toISOString(post.timestamp);
+//         }
+//         if (post.hasOwnProperty('edited')) {
+//             post.editedISO = post.edited !== 0 ? utils.toISOString(post.edited) : '';
+//         }
+//     }
+// }
 function modifyPost(post, fields) {
     if (post) {
         db.parseIntFields(post, intFields, fields);
